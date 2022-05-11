2ed4849c5fc24fb52cbea7dd30ee319ce210eb03
SabrinaPVaz <88401720+SabrinaPVaz@users.noreply.github.com>
2022-05-11 10:24:25 -0300

    Update beecrowd2602.sql

--- beecrowd2602.sql
+++ beecrowd2602.sql
@@ -76,9 +76,65 @@ SELECT MAX(price), MIN(price)
 FROM products
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-/*
+/* 
+O setor financeiro da empresa precisa de um relatório que mostre o código e o nome dos produtos cujo preço são menores que 10 ou maiores que 100.
+
+Tabelas
+products
+id	name	              amount	price
+1	  Two-door wardrobe	  100	      80
+2	  Dining table	      1000	    560
+3	  Towel holder	      10000	    5.50
+4	  Computer desk	      350	      100
+5	  Chair	              3000	    210.64
+6	  Single bed	        750	      99
+*/
+SELECT id, name 
+FROM products 
+WHERE price<10 OR price>100
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* 
+A locadora tem objetivos de criar várias franquias espalhadas pelo Brasil. Para isso queremos saber em quais cidades nossos clientes moram.
+
+Para você nos ajudar selecione o nome de todas as cidades onde a locadora tem clientes. Mas por favor, não repita o nome da cidade.
+
+Tabelas
+customers
+id	name	                        street	                        city
+1	  Giovanna Goncalves Oliveira	  Rua Mato Grosso               	Canoas
+2	  Kauã Azevedo Ribeiro	        Travessa Ibiá	                  Uberlândia
+3	  Rebeca Barbosa Santos        	Rua Observatório Meteorológico	Salvador
+4	  Sarah Carvalho Correia        Rua Antônio Carlos da Silva	    Uberlândia
+5	  João Almeida Lima	            Rua Rio Taiuva	                Ponta Grossa
+6	  Diogo Melo Dias	              Rua Duzentos e Cinqüenta	      Várzea Grande
+*/
+SELECT DISTINCT city
+FROM customers
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* 
+O setor financeiro encontrou alguns problemas na entrega de um dos nossos fornecedores, a entrega dos produtos não condiz com a nota fiscal.
+
+Seu trabalho é exibir o nome dos produtos e o nome do fornecedor, para os produtos fornecidos pelo fornecedor ‘Ajax SA’.
+
+Tabelas
+
+providers
+id	  name	        street	                    city	           state
+1	    Ajax SA	      Presidente Castelo Branco 	Porto Alegre	     RS
+2	    Sansul SA	    Av Brasil	                  Rio de Janeiro	   RJ
+3	    South Chairs	Av Moinho	                  Santa Maria   	   RS
+4	    Elon Electro	Apolo	                      São Paulo   	     SP
+5	    Mike Electro	Pedro da Cunha	            Curitiba	         PR
 
 
-
-
+products
+id	    name	           amount	    value	    id_providers
+1	      Blue Chair	       30	      300.00	      5
+2	      Red Chair	         50	      2150.00	      1
+3	      Disney Wardrobe	   400	    829.50	      4
+4	      Blue Toaster	     20	      9.90	        3
+5	      Solar Panel	       30	      3000.25	      4 
+*/
 
